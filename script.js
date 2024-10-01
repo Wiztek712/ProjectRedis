53de7a981d555ab891e0bcbfa6afb92b60755940
Wiztek712 <raban.quentin@gmail.com>
2024-10-01 18:42:36 +0200

    test script fixed

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-// Import Redis
-const redis = require('redis');
+import { createClient } from 'redis';
 
-// Create a Redis client
-const client = redis.createClient();
+const client = await createClient()
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
 
-// Handle connection events
-client.on('connect', function () {
-  console.log('Connected to Redis...');
-});
-
-client.on('error', function (err) {
-  console.log('Redis error: ' + err);
-});
-
-// Example operations: Set and Get key-value
-client.set('name', 'John Doe', function (err, reply) {
-  if (err) throw err;
-  console.log('SET:', reply);
-  
-  // Retrieve the value for the key 'name'
-  client.get('name', function (err, reply) {
-    if (err) throw err;
-    console.log('GET:', reply);
-
-    // Close the connection
-    client.quit();
-  });
-});
+await client.set('key', 'value');
+const value = await client.get('key');
+await client.disconnect();
