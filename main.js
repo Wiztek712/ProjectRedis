aee86230c679b3dd9746b8d304772fe171c9fafb
Miizzer <clement.jacquet.42600@gmail.com>
2024-10-02 10:56:08 +0200

    Adding operators from F1 grid
    Smooth operatooor

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 import { addCall, addOperator } from "./script.js";
 
-addOperator('utilisateur:1001', "Griezmann", "Antoine");
-addCall('call:1001', '8h12', '0781881212', 'En cours', '75', 'utilisateur:1001', 'Appel pour une pizza')
+addOperator('Operator:44', 'Hamilton', 'Lewis')
+addOperator('Operator:63', 'Russell', 'George')
+
+/*addOperator('Operator:16', 'Leclerc', 'Charles')
+addOperator('Operator:55', 'Sainz', 'Carlos')
+
+addOperator('Operator:33', 'Verstappen', 'Max')
+addOperator('Operator:11', 'Perez', 'Sergio')
+
+addOperator('Operator:10', 'Gasly', 'Pierre')
+addOperator('Operator:31', 'Ocon', 'Esteban')
+
+addOperator('Operator:14', 'Alonso', 'Fernando')
+addOperator('Operator:18', 'Stroll', 'Lance')
+
+addOperator('Operator:4', 'Norris', 'Lando')
+addOperator('Operator:81', 'Piastri', 'Oscar')
+
+addOperator('Operator:22', 'Tsunoda', 'Yuki')
+addOperator('Operator:40', 'Lawson', 'Liam')
+
+addOperator('Operator:24', 'Zhou', 'Guanyu')
+addOperator('Operator:77', 'Bottas', 'Valtteri')
+
+addOperator('Operator:23', 'Albon', 'Alexander')
+addOperator('Operator:43', 'Colapinto', 'Franco')
+
+addOperator('Operator:27', 'Hulkenberg', 'Nico')
+addOperator('Operator:20', 'Magnussen', 'Kevin')*/
+
+
+addCall('call:1', '8h12', '0781881212', 'En cours', '75', 'utilisateur:44', 'Pole position')
